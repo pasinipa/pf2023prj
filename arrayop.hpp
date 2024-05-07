68e99b9c11ad8e63f0ad4001204b2c4c479a1ecf
dsdesmond <167629040+dsdesmond@users.noreply.github.com>
2024-05-07 17:46:58 +0200

    Update arrayop.hpp

--- arrayop.hpp
+++ arrayop.hpp
@@ -10,11 +10,13 @@ operator- (std::array<double, 2> const& a1, std::array<double, 2> const& a2) {
     return { a1[0] - a2[0], a1[1] - a2[1] };
 }
 
-inline std::array<double, 2> operator/ (std::array<double, 2> const& a1, double d) {
+inline std::array<double, 2> 
+operator/ (std::array<double, 2> const& a1, double d) {
     return { a1[0] / d, a1[1] / d };
 }
 
-inline std::array<double, 2> operator* (std::array<double, 2> const& a1, double d) {
+inline std::array<double, 2> 
+operator* (std::array<double, 2> const& a1, double d) {
     return { a1[0] * d, a1[1] * d };
 }
 
