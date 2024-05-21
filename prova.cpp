53bc0f2e71e0490c4284e23465c97c878af55a34
FabrizioMetta <fabriziometta04@gmail.com>
2024-05-21 15:07:51 +0200

    Create prova.cpp

---
+++ prova.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <random>
+
+using ArrayD2 = std::array<double, 2>;
+
+int main() {
+    std::default_random_engine engPos;
+    std::normal_distribution<double> dist1{50., 15.};
+    double a = dist1(engPos);
+    double b = dist1(engPos);
+
+    std::default_random_engine engVel;
+    std::normal_distribution<double> dist2{0.5, 0.2};
+    double c = dist2(engVel);
+    double d = dist2(engVel);
+
+    std::cout << a << ' ' <<  b  << ' ' << c << ' ' << d;
+}
