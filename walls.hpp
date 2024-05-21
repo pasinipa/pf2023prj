522855b58ab648c7127f7a304a57725b65862d3e
FabrizioMetta <fabriziometta04@gmail.com>
2024-05-21 14:10:39 +0200

    Create walls.hpp

---
+++ walls.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "boid.hpp"
+
+class Walls {
+    private:
+    ArrayD2 position; // position of the wall
+    ArrayD2 nearWall; // the area in which the boids "feel" the presence of the wall
+
+    // function that checks if the boid hits the wall and modifies its velocity
+    void boidImpact (Boid b, Walls const& w);
+};
