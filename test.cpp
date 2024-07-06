8a8b08aa7449c308238465f5b74f4a2f030b38e4
FabrizioMetta <fabriziometta04@gmail.com>
2024-07-06 17:27:04 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,13 +4,12 @@
 #include <random>
 #define private public
 
-#include "boid.hpp"
 #include "arrayop.hpp"
+#include "boid.hpp"
 #include "simulation.hpp"
 
 SimPars Simulation::parameters;
 auto const& parameters{Simulation::parameters};
-
 
 TEST_CASE("Testing array operations")
 {
@@ -21,7 +20,9 @@ TEST_CASE("Testing array operations")
   auto sum  = a1 + a2;
   auto diff = a1 - a2;
   auto prod = a1 * k;
-  auto quot = a2 / k; // a voler essere precisi bisognerebbe escludere che divida per 0...
+  auto quot =
+      a2
+      / k; // a voler essere precisi bisognerebbe escludere che divida per 0...
 
   CHECK(sum == ArrayF2{-1., 3.});
   CHECK(diff == ArrayF2{3., 3.});
@@ -35,14 +36,62 @@ TEST_CASE("Testing array operations")
   CHECK(a2 == ArrayF2{-1, .0});
 }
 
-std::vector<Boid> testFlock{Boid{{1., 2.}, {0., 1.}}, Boid{{4., 2.}, {0., 1.}},
-                            Boid{{1., 6.}, {0., 1.}}};
-
-Simulation sim{testFlock};
-
 TEST_CASE("TESTING STATISTICS")
 {
-  FlightStatistics stat{sim.gatherData()};
+  SUBCASE("testing with 3 boids")
+  {
+    std::vector<Boid> testFlock{Boid{{1., 2.}, {0., 1.}},
+                                Boid{{4., 2.}, {0., 1.}},
+                                Boid{{1., 6.}, {0., 1.}}};
 
-  std::cout << stat.meanDist << '\n';
+    Simulation sim{testFlock};
+    FlightStatistics stat{sim.gatherData()};
+    CHECK(stat.meanDist == 4);
+
+    CHECK(stat.meanVel[0] == 0.);
+    CHECK(stat.meanVel[1] == 1.);
+
+    CHECK(stat.stdDevDist == doctest::Approx(0.816).epsilon(0.001));
+
+    CHECK(stat.stdDevVel[0] == 0.);
+    CHECK(stat.stdDevVel[1] == 0.);
+  }
+
+  SUBCASE("testing with negative positions")
+  {
+    std::vector<Boid> testFlock{Boid{{-1., 2.}, {0., 1.}},
+                                Boid{{4., -2.}, {0., 1.}},
+                                Boid{{1., -6.}, {0., 1.}}};
+
+    Simulation sim{testFlock};
+    FlightStatistics stat{sim.gatherData()};
+    CHECK(stat.meanDist == doctest::Approx(6.550).epsilon(0.001));
+
+    CHECK(stat.meanVel[0] == 0.);
+    CHECK(stat.meanVel[1] == 1.);
+
+    CHECK(stat.stdDevDist == doctest::Approx(1.329).epsilon(0.001));
+
+    CHECK(stat.stdDevVel[0] == 0.);
+    CHECK(stat.stdDevVel[1] == 0.);
+  }
+
+  SUBCASE("testing with negative velocities")
+  {
+    std::vector<Boid> testFlock{Boid{{1., 2.}, {0., -1.}},
+                                Boid{{4., 2.}, {1., -4.}},
+                                Boid{{1., 6.}, {0., 1.}}};
+
+    Simulation sim{testFlock};
+    FlightStatistics stat{sim.gatherData()};
+    CHECK(stat.meanDist == 4);
+
+    CHECK(stat.meanVel[0] == doctest::Approx(0.333).epsilon(0.001));
+    CHECK(stat.meanVel[1] == doctest::Approx(-1.333).epsilon(0.001));
+
+    CHECK(stat.stdDevDist == doctest::Approx(0.816).epsilon(0.001));
+
+    CHECK(stat.stdDevVel[0] == doctest::Approx(0.471).epsilon(0.001));
+    CHECK(stat.stdDevVel[1] == doctest::Approx(2.055).epsilon(0.001));
+  }
 }
